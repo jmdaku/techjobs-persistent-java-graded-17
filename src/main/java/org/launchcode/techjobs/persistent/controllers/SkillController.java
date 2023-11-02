eed5603c0b2dc70927fddb7bb1724d7a42160cb9
jmdaku <jmdaku@gmail.com>
2023-11-01 22:25:06 -0400

    added skill controller & updated employer controller

---
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
@@ -0,0 +1,31 @@
+package org.launchcode.techjobs.persistent.controllers;
+
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class SkillController {
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @GetMapping("add")
+    public String displayAddSkillForm(Model model) {
+        model.addAttribute(new Skill());
+        return "skill/add";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
